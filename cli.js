861c38d43905367828cb6ee8120cfec26c62660b
Dominykas Blyžė <hello@dominykas.com>
2015-06-06 19:05:06 +0300

    log unhandled rejections

--- cli.js
+++ cli.js
@@ -4,6 +4,10 @@ if (!fn) {
 	throw new Error("File name not provided");
 }
 
+process.on("unhandledRejection", function (err) {
+	console.warn("Unhandled rejection:", err, "\n", err.stack);
+});
+
 require("./index.js")(fn, function (err) {
 	if (err) throw err;
 	console.log("OK");
